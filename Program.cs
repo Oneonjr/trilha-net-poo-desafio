ddb886a3c11571ddb5272263d08f09115fb89d43
Oneonjr <oneonjunior@outlook.com>
2022-10-24 21:37:52 -0300

    Primeiro commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("123456789", "Modelo 01", "11111111111", 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Baixou o WhatsApp pela Play Store");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone("987654321", "Modelo 10", "22222222222", 64);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Baixou o Telegram pelo App Store");
